19585cd106749d3b89b6e3d3c2f2ff31af6ee415
Spanky(Mac) <819062398@qq.com>
2021-04-27 15:12:01 +0800

    页面链接

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 本地运行: npm run dev  
 localhost:8080
+
+CODE 无法输入执行npm
+在Windows应用中找到Windows PowerShell，以管理员运行：
+在命令框输入： set-ExecutionPolicy RemoteSigned， 然后输入A即可解决
--- iframepage.js
+++ iframepage.js
@@ -1,20 +1,32 @@
+const server_origin = 'http://47.108.151.252';		   // 正式环境origin
+const isProduction = location.origin == server_origin; // 是否是正式环境
+
+const menus = [
+	'数字资产',
+	'协同设计',
+	'智慧工地',
+	'智慧运维',
+];
 const allpages = [
 	{
 		name: '首页',
-		urlleft:'home/left.html',
-		urlmiddle:'./map/index.html',
-		leftwidth: '365px',
-		rightwidth: '500px',
-		urlright:'home/right.html',
-		fzr:''
-	},
-	{
-		name: '数字资产',
 		urlleft:'szzc/left.html',
-		urlmiddle:'http://localhost:9530/Apps/Demos/Viewer/index.html',
+		urlmiddle:'./map/index.html',
 		leftwidth: '480px',
 		rightwidth: '480px',
 		urlright:'szzc/right.html',
-		fzr:''
+		params: {
+			left: {
+				videoSrc: (isProduction ? './video/videoplayback (3).mp4' : (server_origin + '/szzc/video/videoplayback (3).mp4'))
+			}
+		}
+	},
+	{
+		name: '开州至梁平高速公路',
+		urlleft:'home/left.html',
+		urlmiddle: (isProduction ? '' : server_origin)  + '/Viewer/',
+		leftwidth: '365px',
+		rightwidth: '500px',
+		urlright:'home/right.html'
 	}
 ]
